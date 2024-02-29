f0d11c69779e8ff3bc91af53581bb6a4213c1f95
Jal <106935663+JAL117@users.noreply.github.com>
2024-02-29 05:59:09 -0600

    Consumer

---
+++ consumer.ts
@@ -0,0 +1,34 @@
+import amqp from 'amqplib';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function getEvent() {
+    const url = process.env.URL ?? "";
+    const conn = await amqp.connect(url);
+    const channel = await conn.createChannel();
+
+    const exchange = 'Prueba';
+
+    await channel.assertExchange(exchange, 'direct', {durable: true});
+
+    const queueName = 'initial';
+    const queue = await channel.assertQueue(queueName, {exclusive: false});
+    await channel.bindQueue(queue.queue, exchange, '12345');
+
+    console.log('Escuchando eventos en RabbitMQ');
+
+    channel.consume(queue.queue, async(mensaje)=>{
+        if(mensaje !== null){
+            console.log(`Mensaje recibido: ${mensaje.content.toString()}`);
+            try {
+                const id = Number(mensaje.content);
+                const response = await axios.post('http://localhost:9000/history',{id_user:id, registration:"Usuario Registrado"});
+                
+            } catch (error) {
+                console.log("Error sending to API");   
+            }
+        }
+    }, {noAck:true});
+}
+getEvent().catch(console.error);
